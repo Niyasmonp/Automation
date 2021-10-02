3b45b45818749d54f9e6a7f6547a3b350ed5907b
Niyas Mon P <niyasmonp.india@gmail.com>
2021-10-03 00:28:08 +0530

    comments added

--- screenshort.py
+++ screenshort.py
@@ -1,14 +1,14 @@
-import serial
-import pyautogui
+import serial    #library for serail communication
+import pyautogui #library to automate user interface
 import time
 ser=serial.Serial('COM6',9600)
 dist=100
-def ss():
-    pyautogui.hotkey("win","prtsc")
+def ss():  #function to take screenshot
+    pyautogui.hotkey("win","prtsc") 
     time.sleep(0.2)
 while True:
-    distance=ser.readline().strip()
-    dist=int(float(distance.decode()))
+    distance=ser.readline().strip() #Reading the input from serial port
+    dist=int(float(distance.decode())) #.decode() to conver 'byte' into 'str'
     print(dist)
     if dist < 10 :
         ss()
