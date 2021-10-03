cddfcb851759cbcfc185481cc524eaf8b75f9bad
Niyas Mon P <niyasmonp.india@gmail.com>
2021-10-03 11:47:41 +0530

    Python file added

---
+++ Auto Join_meet/Gmeet_auto.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+import webbrowser
+import  schedule
+def join_gmeet():
+    webbrowser.open("https://meet.google.com/sfw-espv-bcm?pli=1&authuser=1")
+    time.sleep(8)
+    # print(pyautogui.position())
+    # pyautogui.click(1786, 144)
+    # time.sleep(4)
+    # pyautogui.click(879, 533)
+    # time.sleep(5)
+    pyautogui.hotkey("ctrl", "d")
+    pyautogui.hotkey("ctrl", "e")
+    time.sleep(1)
+    pyautogui.click(1348, 594)
+schedule.every().thursday.at("22:00").do(join_gmeet)
+while True:
+    schedule.run_pending()
+    time.sleep(10)
