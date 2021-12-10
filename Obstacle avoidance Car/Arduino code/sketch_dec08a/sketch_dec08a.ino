58de6bcff396aa538b208987fd31c58aa9a5fa57
Niyas Mon P <niyasmonp.india@gmail.com>
2021-12-10 17:07:45 +0530

    Small changes
    Robustness of stop, turn Rt,Lt increased

--- Obstacle avoidance Car/Arduino code/sketch_dec08a/sketch_dec08a.ino
+++ Obstacle avoidance Car/Arduino code/sketch_dec08a/sketch_dec08a.ino
@@ -1,44 +1,66 @@
 #include <ESP32Servo.h>
-Servo servo;
+  Servo servo;
   int trig=32;
   int echo=34;
   int d1,d2,d;
   float distance;
-  void moveForward(){
+
+void moveForward(){
   digitalWrite(25,LOW);
   digitalWrite(27,HIGH);
   digitalWrite(12,LOW);
   digitalWrite(13,HIGH);
 } 
+
 void moveReverse(){
   digitalWrite(25,HIGH);
   digitalWrite(27,LOW);
   digitalWrite(12,HIGH);
   digitalWrite(13,LOW);
-  delay(1000);
 }
+
 void turnRight(){
-//  digitalWrite(25,LOW);
-//  digitalWrite(27,LOW);
-//  digitalWrite(12,LOW);
+  digitalWrite(25,HIGH);
+  digitalWrite(27,LOW);
+  digitalWrite(12,LOW);
   digitalWrite(13,HIGH);
+  delay(300);
 }
+
 void turnLeft(){
-//  digitalWrite(25,LOW);
+  digitalWrite(25,LOW);
   digitalWrite(27,HIGH);
-//  digitalWrite(12,LOW);
-//  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(13,LOW);
+  delay(300);
 }
+
 void Stop(){
   digitalWrite(25,LOW);
   digitalWrite(27,LOW);
   digitalWrite(12,LOW);
   digitalWrite(13,LOW);
-  delay(1000);
 }
+
+int getDist(){ 
+    digitalWrite(trig,LOW);
+    delayMicroseconds(2);  
+    digitalWrite(trig,HIGH);
+    delayMicroseconds(10);   
+    digitalWrite(trig,LOW);  
+    float time=pulseIn(echo,HIGH); 
+    distance=time*0.034/2;
+    return distance;
+}
+
 void action(){
-    Stop();
-  servo.write(15);
+  Stop();
+  delay(100);
+  moveReverse();
+  delay(500);
+  Stop();
+  delay(200);
+  servo.write(10);
   delay(500);
   d1=getDist();
   delay(300); 
@@ -50,20 +72,7 @@ void action(){
   delay(300);
   servo.write(90);
 }
-int getDist(){
-    digitalWrite(trig,LOW);
-    delayMicroseconds(2);
-  
-    digitalWrite(trig,HIGH);
-    delayMicroseconds(10);
-    
-    digitalWrite(trig,LOW);
-  
-    float time=pulseIn(echo,HIGH);
-  
-    distance=time*0.034/2;
-    return distance;
-  }
+
 void setup() {
   servo.attach(5);
   servo.write(90);
@@ -74,23 +83,29 @@ void setup() {
   pinMode(5,OUTPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo,INPUT);
-  Serial.begin(9600);
-  
+  Serial.begin(9600);  
 }
 
 void loop() {
     delay(500);
     d=getDist();
-    if (d>36){moveForward();}
+  
+  if (d>35){
+    moveForward();
+  }
+  
   else{
     action();
     Serial.println(d1);
     Serial.println(d2);
-  if (d1>=d2){
-    turnRight();
-    }
-  else if(d2>15) {turnLeft();
-    }
-//  else{moveReverse();}
-    delay(330);}
+    if (d1>=d2){
+      turnRight();
+      Stop();
+      }
+    else if(d2>15) {turnLeft();
+    Stop();
+      }
+    delay(330);
+  }
+  
 }
